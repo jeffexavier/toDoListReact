4818fa3913b781d78ceec12e8a6690239a1490ec
Ana Alura <jeffexavier@gmail.com>
2023-03-26 01:43:14 -0300

    criação de arquivos com funcoes especificas, e separação, novamente, dos componentes form e listas

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from "react";
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Menu from './components/Menu.jsx'
-import ListaTarefas from "./components/Lista.jsx";
+import ListaTarefas from "./components/ItemList.jsx";
+import AddItemForm from "./components/AddItemForm.jsx"
 
 import './style/Body.css';
-
 
 export default function App() {
   
@@ -15,12 +15,13 @@ export default function App() {
 
       <Menu />
       <Routes>
-        <Route exact path="/teste "element={
+        <Route exact path="/teste"element={
           <h1>Teste do Jeffinho</h1>
         } />
         <Route />
         <Route exact path="/" element={
               <div className="body">
+              <AddItemForm />
               <ListaTarefas />
               </div>
         }/>
